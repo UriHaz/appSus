15e04f182fae92f5e37e0eae2b8674aa725cb012
Naama Shoshani <naamashoshani@gmail.com>
2020-06-27 22:34:15 +0300

    2234

--- main-app/js/home-page-cmp.js
+++ main-app/js/home-page-cmp.js
@@ -6,8 +6,7 @@ export default {
             <h1>Appsus by Uri & Na'ama</h1>
         </div>
         
-        <img src="../../img/unicorn.svg" class="unicorn" width="400" height="400">
-        <img src="https://imgur.com/JkuUdSr" class=unicorn">
+        <router-link class="unicorn"> </router-link>
     </section>
     `,
 };
