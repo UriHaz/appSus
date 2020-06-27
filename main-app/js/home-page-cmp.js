6b7f89eb2ea9948061035efb216fab7c0e6b3b57
Naama Shoshani <naamashoshani@gmail.com>
2020-06-27 23:09:54 +0300

    2309

--- main-app/js/home-page-cmp.js
+++ main-app/js/home-page-cmp.js
@@ -4,7 +4,7 @@ export default {
 
         <div class="home-page-content">
             <h1>Appsus by Uri & Na'ama</h1>
-            <p class="fas fa-chess-knight"></p>
+            <i class="fas fa-chess-knight"></i>
         </div>
         
         <img src="img/unicorn.png" class="unicorn" width="400" height="400">
